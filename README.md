855a8c8de844ba63d42d93311481d003721ef179
Robert Southee <rksouthee@gmail.com>
2021-02-15 12:57:43 +0000

    Write a simple image using PPM

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # ray-tracer
 
-A basic ray tracer
+A basic ray tracer.
+
+I generate an image in the [Netbpm](https://en.wikipedia.org/wiki/Netpbm) format.
 
 ## Building
 
@@ -12,3 +14,12 @@ ray-tracer>cmake-gui .
 ray-tracer>rem configure and generate
 ray-tracer>cmake --build build
 ```
+
+## Running
+
+```bat
+ray-tracer>ray > image.ppm
+```
+
+The result image `image.ppm` can then be opened by an editor. I used
+[OpenSeeIt](http://openseeit.sourceforge.net/).
--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,14 @@
 
 int main()
 {
-	std::cout << "Hello, world!" << std::endl;
+	// Write an PPM image to stdout (example from wikipedia)
+	std::cout << "P3\n"; // RGB color in ASCII
+	std::cout << "3 2\n"; // 3 pixels wide and 2 pixels high
+	std::cout << "255\n"; // maximum value for each color
+	std::cout << "255 0 0\n"; // red
+	std::cout << "0 255 0\n"; // green
+	std::cout << "0 0 255\n"; // blue
+	std::cout << "255 255 0\n"; // yellow
+	std::cout << "255 255 255\n"; // white
+	std::cout << "0 0 0\n"; // black
 }
